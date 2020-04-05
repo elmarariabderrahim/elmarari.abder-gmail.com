7d731a169792bc8268812dcbe584ad6af5b7c0f5
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-04-05 22:47:18 +0100

    Update add_to_container.sh

--- add_to_container.sh
+++ add_to_container.sh
@@ -41,21 +41,7 @@ for f in sql_scripts/*; do
 	mysql -uroot -ppixid123 -Bse "use db5;insert into scripts (script_name,script_state) values('$script_name','failed');"
  fi
 done
-echo $flag
-if [ $flag = 'false' ]; then 
- echo " script incorecte"
- else
-IFS=':'
-for f in sql_scripts/*; do
-input="./$f"
-varrr=""
-while IFS= read -r line
-do
-varrr="${varrr}$line"
-done < "$input"
-mysql -uroot -ppixid123 -Bse "$varrr"
-done
-fi
+
 
 
 
