002801cc5c5d6ba3defba00bfa5c162e94c61e2c
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-04-05 18:16:12 +0100

    Update add_to_container.sh

--- add_to_container.sh
+++ add_to_container.sh
@@ -9,7 +9,7 @@ mysql -P $docker_mysql_port --protocol=tcp -u root -ppixid123
 
 path=$(pwd)
 echo $path
-input="${docker_mysql_port}/logfile.txt"
+input="$path/logfile.txt"
 var=""
 while IFS= read -r line
 do
